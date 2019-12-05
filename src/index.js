0145a4ec9d91e5516d443faf3064b12efcb5814b
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2019-12-05 11:57:39 +0200

    output method

--- src/index.js
+++ src/index.js
@@ -3,26 +3,30 @@ const formatTime = require('./formatTime')
 const Stopwatch = class {
   
   constructor(cb, options = {}) {
-    this.callback(cb)
-
+    
     this._playing = false
     this.start = 0
     this.stop = 0
     this.format = 'hh:mm:ss.d0'
-    this._max = 100 * 60 * 60 * 1000 - 1 // 359999999
+    this.max = 100 * 60 * 60 * 1000 - 1 // 359999999
     this._callback = () => {}
     this.updateTime = 50
     this._steps
-
+    
+    this.callback(cb)
     Object.assign(this, options)
   }
   
-  get ms() {return (this.play ? new Date().getTime() : this.stop) - this.start}
+  get ms() {return (this.playing ? new Date().getTime() : this.stop) - this.start}
   
   get formatted() {return formatTime(this.ms, this.format)}
 
   update(newState) {
-    Object.assign(this, newState)
+    const {playing, ...rest} = newState
+    Object.assign(this, rest)
+    this._playing != playing ?
+      this.playing = playing :
+      this._callback(this)
   }
 
   callback(cb) {
@@ -31,18 +35,24 @@ const Stopwatch = class {
     }
   }
 
+  output() {
+    return (({playing, start, stop, format, updateTime, max}) => ({playing, start, stop, format, updateTime, max}))(this)
+  }
+
   set playing(state) {
-    this._playing = state != 0
-    if (this._playing) {
-      clearInterval(this._steps)
-      this._steps = setInterval(() => {
-        this._callback(this)
-      }, this.updateTime)
-    } else  {
-      clearInterval(this._steps)
+    if (this._playing != state) {
+      this._playing = state != 0
+      if (this._playing) {
+        clearInterval(this._steps)
+        this._steps = setInterval(() => {
+          this._callback(this)
+        }, this.updateTime)
+      } else  {
+        clearInterval(this._steps)
+      }
+      this._callback(this)
+      return this._playing
     }
-    this._callback(this)
-    return this._playing
   }
 
   get playing() {
@@ -58,18 +68,19 @@ const Stopwatch = class {
       this.stop = now
     }
     this.playing = state != 0
-    return this
+    return this.output()
   }
 
   reset(ms) {
     if (ms != undefined) {
-      ms = Math.round(Math.min(Math.max(ms, -this._max), this._max))
+      ms = Math.round(Math.min(Math.max(ms, -this.max), this.max))
       this.stop = new Date().getTime()
       this.start = this.stop - ms
     } else {
       this.start = this.stop = new Date().getTime()
     }
-    return this
+    this._callback(this)
+    return this.output()
   }
   
 }
