83559e04b27cf87f617e69ae00bf24581be4778f
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2020-01-19 04:34:53 +0200

    offset Added: offset method, eslint

--- README.md
+++ README.md
@@ -4,3 +4,162 @@ A stopwatch class based on start time, with state output and state update method
 
 ## Installation
 `npm i dimitriaatos/syncwatch`
+
+## Documentation
+
+**Kind**: global class  
+
+* [Syncwatch](#Syncwatch)
+    * [new Syncwatch(cb, options)](#new_Syncwatch_new)
+    * [.start](#Syncwatch+start)
+    * [.stop](#Syncwatch+stop)
+    * [.format](#Syncwatch+format)
+    * [.max](#Syncwatch+max)
+    * [.updateTime](#Syncwatch+updateTime)
+    * [.ms](#Syncwatch+ms)
+    * [.formatted](#Syncwatch+formatted)
+    * [.playing](#Syncwatch+playing)
+    * [.update(newState)](#Syncwatch+update)
+    * [.callback(cb)](#Syncwatch+callback)
+    * [.output()](#Syncwatch+output) ⇒ <code>object</code>
+    * [.toggle(playing)](#Syncwatch+toggle)
+    * [.reset(ms)](#Syncwatch+reset)
+    * [.play()](#Syncwatch+play) ⇒ <code>object</code>
+    * [.pause()](#Syncwatch+pause) ⇒ <code>object</code>
+    * [.stop()](#Syncwatch+stop) ⇒ <code>object</code>
+
+<a name="new_Syncwatch_new"></a>
+
+### new Syncwatch(cb, options)
+
+| Param | Type | Description |
+| --- | --- | --- |
+| cb | <code>func</code> | A callback function to be triggered repetitively when the time changes. |
+| options | <code>object</code> | Initialize any property. |
+
+**Example**  
+```js
+import Syncwatch from 'syncwatch'
+const watch = new Syncwatch(watch => console.log(watch.formatted), {format: 'hh/mm/ss'})
+watch.start()
+```
+<a name="Syncwatch+start"></a>
+
+### syncwatch.start
+The starting time of the syncwatch (default: 0).
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+stop"></a>
+
+### syncwatch.stop
+The stopping time, where the time is calculated from when the syncwatch is paused (default: 0).
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+format"></a>
+
+### syncwatch.format
+The format to use to generate the formatted time (default: hh:mm:ss.d0).
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+max"></a>
+
+### syncwatch.max
+The maximum time (default: 359999999), when the syncwatch exceeds it will go back to 0.
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+updateTime"></a>
+
+### syncwatch.updateTime
+The time interval between the invoking of the callback function
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+ms"></a>
+
+### syncwatch.ms
+The current time in ms.
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+formatted"></a>
+
+### syncwatch.formatted
+The current time in the specified form.
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+playing"></a>
+
+### syncwatch.playing
+A property setter/getter for the syncwatch's playing state, this is useful for toggling syncwatch without triggering a function, when getting an external state.
+
+**Kind**: instance property of [<code>Syncwatch</code>](#Syncwatch)  
+<a name="Syncwatch+update"></a>
+
+### syncwatch.update(newState)
+Update the state of the syncwatch.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| newState | <code>object</code> | A new state. |
+
+<a name="Syncwatch+callback"></a>
+
+### syncwatch.callback(cb)
+Passing a callback function.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| cb | <code>function</code> | A callback function to be triggered repetitively when the time changes. |
+
+<a name="Syncwatch+output"></a>
+
+### syncwatch.output() ⇒ <code>object</code>
+Getting the state of the syncwatch.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+**Returns**: <code>object</code> - state.  
+<a name="Syncwatch+toggle"></a>
+
+### syncwatch.toggle(playing)
+An on/off toggle for the syncwatch.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| playing | <code>bool</code> | state |
+
+<a name="Syncwatch+reset"></a>
+
+### syncwatch.reset(ms)
+Resetting the time of the syncwatch.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| ms | <code>number</code> | New time. |
+
+<a name="Syncwatch+play"></a>
+
+### syncwatch.play() ⇒ <code>object</code>
+Launching the syncwatch.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+**Returns**: <code>object</code> - state.  
+<a name="Syncwatch+pause"></a>
+
+### syncwatch.pause() ⇒ <code>object</code>
+Pausing the syncwatch.
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+**Returns**: <code>object</code> - state.  
+<a name="Syncwatch+stop"></a>
+
+### syncwatch.stop() ⇒ <code>object</code>
+Stopping the syncwatch (goes back to 0).
+
+**Kind**: instance method of [<code>Syncwatch</code>](#Syncwatch)  
+**Returns**: <code>object</code> - state.  
--- src/index.js
+++ src/index.js
@@ -1,107 +1,172 @@
 const formatTime = require('./formatTime')
 
-const Stopwatch = class {
+/**
+ * A stopwatch class.
+ * 
+ * @example import Syncwatch from 'syncwatch'
+ * const watch = new Syncwatch(watch => console.log(watch.formatted), {format: 'hh/mm/ss'})
+ * watch.start()
+ */
+
+const Syncwatch = class {
   
-  constructor(cb, options = {}) {
-    
-    this._playing = false
-    this.start = 0
-    this.stop = 0
-    this.format = 'hh:mm:ss.d0'
-    this.max = 100 * 60 * 60 * 1000 - 1 // 359999999
-    this._callback = () => {}
-    this.updateTime = 50
-    this._steps
-    
-    this.callback(cb)
-    Object.assign(this, options)
-  }
+	/**
+   * @param {func} cb - A callback function to be triggered repetitively when the time changes.
+   * @param {object} options - Initialize any property.
+   */
+	constructor(cb, options = {}) {
+
+		/* This property holds the playing state (on/off) of the stopwatch */
+		this._playing = false
+
+		/** The starting time of the syncwatch (default: 0). */
+		this.start = 0
+
+		/** The stopping time, where the time is calculated from when the syncwatch is paused (default: 0). */
+		this.stop = 0
+
+		/** The format to use to generate the formatted time (default: hh:mm:ss.d0). */
+		this.format = 'hh:mm:ss.d0'
+
+		/** The maximum time (default: 359999999), when the syncwatch exceeds it will go back to 0. */
+		this.max = 100 * 60 * 60 * 1000 - 1
+
+		/** The time interval between the invoking of the callback function */
+		this.updateTime = 50
+
+		/* The callback function to be called when the time changes */
+		this._callback = () => {}
+
+		/* This property will hold the ID value returned by the setInterval() that will trigger the time changes */
+		this._steps
+
+		/* A time offset */
+		this.offset = 0
+
+		// Overwriting default values with the options given to the constructor.
+		this.callback(cb)
+		Object.assign(this, options)
+		
+	}
   
-  get ms() {return (this.playing ? Date.now() : this.stop) - this.start}
+	/** The current time in ms. */
+	get ms() {
+		return (this.playing ? Date.now() + this.offset : this.stop) - this.start
+	}
   
-  get formatted() {return formatTime(this.ms, this.format)}
+	/** The current time in the specified form. */
+	get formatted() {return formatTime(this.ms, this.format)}
 
-  update(newState) {
-    const {playing, ...rest} = newState
-    Object.assign(this, rest)
-    this._playing != playing ?
-      this.playing = playing :
-      this._callback(this)
-  }
+	/**
+   * Update the state of the syncwatch.
+   * @param {object} newState - A new state.
+   */
+	update(newState) {
+		const {playing, ...rest} = newState
+		Object.assign(this, rest)
+		this._playing != playing ?
+			this.playing = playing :
+			this._callback(this)
+	}
 
-  callback(cb) {
-    this._callback = (t) => {
-      cb && cb(t)
-    }
-  }
+	/**
+   * Passing a callback function.
+   * @param {function} cb - A callback function to be triggered repetitively when the time changes.
+   */
+	callback(cb) {
+		this._callback = (t) => {
+			cb && cb(t)
+		}
+	}
 
-  output() {
-    return (({playing, start, stop, format, updateTime, max}) => ({playing, start, stop, format, updateTime, max}))(this)
-  }
+	/**
+   * Getting the state of the syncwatch.
+   * @return {object} state.
+   */
+	output() {
+		return (({playing, start, stop, format, updateTime, max}) => ({playing, start, stop, format, updateTime, max}))(this)
+	}
 
-  set playing(state) {
-    if (this._playing != state) {
-      this._playing = state != 0
-      if (this._playing) {
-        clearInterval(this._steps)
-        this._steps = setInterval(() => {
-          this._callback(this)
-        }, this.updateTime)
-      } else  {
-        clearInterval(this._steps)
-      }
-      this._callback(this)
-      return this._playing
-    }
-  }
+	/**
+   * A property setter/getter for the syncwatch's playing state, this is useful for toggling syncwatch without triggering a function, when getting an external state.
+   */
+	get playing() {
+		return this._playing
+	}
+  
+	set playing(state) {
+		if (this._playing != state) {
+			this._playing = state != 0
+			if (this._playing) {
+				clearInterval(this._steps)
+				this._steps = setInterval(() => {
+					this._callback(this)
+				}, this.updateTime)
+			} else  {
+				clearInterval(this._steps)
+			}
+			this._callback(this)
+			return this._playing
+		}
+	}
 
-  get playing() {
-    return this._playing
-  }
+	/**
+   * An on/off toggle for the syncwatch.
+   * @param {bool} playing state
+   */
+	toggle(state) {
+		const now = Date.now()
+		if (state) {
+			this.start = now - (this.stop - this.start)
+			this.stop = undefined
+		} else {
+			this.stop = now
+		}
+		this.playing = state != 0
+		return this.output()
+	}
 
-  toggle(state) {
-    const now = Date.now()
-    if (state) {
-      this.start = now - (this.stop - this.start)
-      this.stop = undefined
-    } else {
-      this.stop = now
-    }
-    this.playing = state != 0
-    return this.output()
-  }
+	/**
+   * Resetting the time of the syncwatch.
+   * @param {number} ms - New time.
+   */
+	reset(ms) {
+		if (ms != undefined) {
+			ms = Math.round(Math.min(Math.max(ms, -this.max), this.max))
+			this.stop = Date.now()
+			this.start = this.stop - ms
+		} else {
+			this.start = this.stop = Date.now()
+		}
+		this._callback(this)
+		return this.output()
+	}
 
-  reset(ms) {
-    if (ms != undefined) {
-      ms = Math.round(Math.min(Math.max(ms, -this.max), this.max))
-      this.stop = Date.now()
-      this.start = this.stop - ms
-    } else {
-      this.start = this.stop = Date.now()
-    }
-    this._callback(this)
-    return this.output()
-  }
+	/**
+   * Launching the syncwatch.
+   * @return {object} state.
+   */
+	play() {
+		return this.toggle(true)
+	}
 
-  play() {
-    return this.toggle(true)
-  }
+	/**
+   * Pausing the syncwatch.
+   * @return {object} state.
+   */
+	pause() {
+		return this.toggle(false)
+	}
 
-  pause() {
-    return this.toggle(false)
-  }
-
-  stop() {
-    this.toggle(false)
-    return this.reset()
-  }
+	/**
+   * Stopping the syncwatch (goes back to 0).
+   * @return {object} state.
+   */
+	stop() {
+		this.toggle(false)
+		return this.reset()
+	}
   
 }
 
-module.exports = Stopwatch
-
-// const stopwatch = new Stopwatch(time => {
-//   console.log(time.formated)
-// })
-
-// stopwatch.play(true)
+module.exports = Syncwatch
